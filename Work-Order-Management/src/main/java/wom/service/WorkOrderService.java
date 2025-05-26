aff94dc5a3f58d2ec72dd1da7de126538aed0561
2403679 <2403679@LTIN588862.cts.com>
2025-05-26 14:29:44 +0530

    work order management updated

--- Work-Order-Management/src/main/java/wom/service/WorkOrderService.java
+++ Work-Order-Management/src/main/java/wom/service/WorkOrderService.java
@@ -10,9 +10,7 @@ public interface WorkOrderService {
 	WorkOrder createWorkOrder(WorkOrder workOrder);
 	List<WorkOrder> getWorkOrdersByStatus(String Status);
 	WorkOrder updateWorkOrderStatus(String WorkOrderId, String Status) throws Exception;
-	WorkOrder updateWorkOrder(String workOrderId, WorkOrder workOrder) throws Exception;
 	List<WorkOrder> getAllWorkOrders();
 	WorkOrder getWorkOrdersByWorkOrderId(String WorkOrderId);
-	//WorkOrder getWorkOrdersByPlanID(String PlanId);
-   
+
 }
